4b2deca7ce1f193809cab5c9a5c903f326aab87e
Avinashradadiya <avinashradadiya21@gmail.com>
2023-03-02 16:09:22 +0530

    new commit

---
+++ MVC/CI-Platform/CI-Platform/Controllers/ResetPasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CI_platform.Controllers
+{
+    public class ResetPasswordViewModel
+    {
+
+        public string Password { get; set; } = null!;
+
+        public string ConfirmPassword { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Token { get; set; } = null!;
+    }
+}
